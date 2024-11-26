ee0ee3af1369f224e616b5dc1702b65286647342
Dawson <hello@dawson.gg>
2024-11-26 16:31:59 -0500

    Correct UpdateStarType type

--- src/types/create-star.ts
+++ src/types/create-star.ts
@@ -15,12 +15,16 @@ export type CreateStarType = {
 }
 
 export type UpdateStarType = {
-  type: starType
+  type?: starType
   customJar?: string;
   version?: string;
-  javaVersion: "8" | "11" | "17" | "21";
-  ports: RequestedPortInfo[];
-  resources: {
-    memory: number;
-  };
+  javaVersion?: "8" | "11" | "17" | "21";
+  ports?: RequestedPortInfo[];
+
+  // Not current implemented, we will support this in the future
+  // We need to handle the payment, how much we charge, etc.
+  // This is easy with hourly billing, hard with monthly.
+  // resources: {
+  //   memory: number;
+  // };
 }

7b63e4123eb5fd9f47f13d980e7a7f7ea61c8052
Dawson <hello@dawson.gg>
2025-01-20 13:47:40 -0500

    Add Port import

--- src/ports/index.ts
+++ src/ports/index.ts
@@ -1,5 +1,6 @@
 import type { AxiosInstance } from "axios";
 import type { CarbonStar } from "../carbon-star";
+import type { Port } from "./types";
 
 export class PortManager {
     private star: CarbonStar;

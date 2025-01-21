c69cf86a9fca3959a9ac04698730c5f8d4a65390
Dawson <hello@dawson.gg>
2025-01-20 22:23:15 -0500

    update types

--- src/file-manager/types.ts
+++ src/file-manager/types.ts
@@ -1,6 +1,7 @@
 export type FileInfo = {
   name: string;
   mode: string;
+  modeBits: string;
   size: number;
   isFile: boolean;
   isSymlink: boolean;
@@ -13,7 +14,7 @@ export type ChmodOptions = {
   path: string,
   files: {
     file: string,
-    mode: number,
+    mode: string,
   }[]
 };
 
